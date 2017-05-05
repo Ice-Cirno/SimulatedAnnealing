f5bf873ec9978c326b3fdf55865c9a9e7cd07c7e
Ice-Cirno <438971718@qq.com>
2017-05-05 12:01:56 +0800

    基本框架

--- [2017-05]SimulatedAnnealing/SA.h
+++ [2017-05]SimulatedAnnealing/SA.h
@@ -1,7 +1,29 @@
 #pragma once
+#define TEMPERATURE_INIT 1000000000
+#define ANEALING_COEFF 0.999
+#define TEMPERATURE_GOAL 0.001
+#define RES 100
+
+enum Max_or_Min { max = -1, min = 1 };
 
 class SA {
+private:
+	double Temperature;
+	bool AcceptBadRoot();
 
+protected:
+	Max_or_Min _max_or_min;
+	void* best_root;
+	void* curr_root;
+	void* new_root;
 
+	virtual void ReadFile() = 0;
+	virtual void Init() = 0;
+	virtual void GenerateNew() = 0;
+	virtual double Estimate(void*) = 0;
 
+public:
+	SA();
+	virtual ~SA();
+	void Do();
 };
